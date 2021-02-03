8e187919b2eac49a1784a152539421d9f27bcefc
eddievanhalen <bruno.silva@gmail.com>
2021-02-03 10:49:53 -0300

    Realizando a importacao do arquivos para o git

---
+++ Yield/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Yield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var resultado = Potencia(9, 2);
+           Teste();
+ 
+        }
+
+
+        public static void Teste() {
+
+            var t = 0;
+        
+        }
+        public static IEnumerable<int> Potencia(int numero, int exponente)
+        {
+            int resultado = 1;
+            for (int i = 0; i < exponente; i++)
+            {
+                resultado = resultado * numero;
+                yield return resultado;
+            }
+        }
+    }
+
+
+
+    
+}
